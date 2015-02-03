f7444f1d3e43da497843ea07dea6b367a083405b
David Cole <dcole@rentpath.com>
2015-02-03 10:17:14 -0500

    [DC/LT] [85945742] Use @select.

--- dist/components/ui/counter.js
+++ dist/components/ui/counter.js
@@ -5,11 +5,11 @@ define(['jquery', 'flight/lib/component', 'swiper'], function($, defineComponent
       totalSelector: '.js-ui-counter-total'
     });
     this.initializeCounter = function(event, data) {
-      $(this.attr.activeSelector).text(1);
-      return $(this.attr.totalSelector).text(data.urls.length);
+      $(this.select('activeSelector')).text(1);
+      return $(this.select('totalSelector')).text(data.urls.length);
     };
     this.updateCounter = function(event, data) {
-      return $(this.attr.activeSelector).text(data.activeIndex + 1);
+      return $(this.select('activeSelector')).text(data.activeIndex + 1);
     };
     return this.after('initialize', function() {
       this.on('dataGalleryContentAvailable', this.initializeCounter);
