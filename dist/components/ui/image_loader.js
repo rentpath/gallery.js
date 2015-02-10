6e05f01a229aebde97d6e1e4f44ebed5e9123e8c
Sam Brauer <sbrauer@rentpath.com>
2015-02-10 15:29:25 -0500

    [SB][88080074] fix image error handling
    Previously the error handler wasn't being called.

--- dist/components/ui/image_loader.js
+++ dist/components/ui/image_loader.js
@@ -4,31 +4,27 @@ define(['jquery', 'flight/lib/component'], function($, defineComponent) {
       errorUrl: void 0,
       lazyLoadThreshold: void 0
     });
-    this.loadImages = function(num) {
-      return this.$node.find("img[data-src]").slice(0, num).each(function() {
-        var img;
-        img = $(this);
-        return img.attr('src', img.attr('data-src')).removeAttr('data-src');
-      });
-    };
     this.initialLoad = function() {
       return this.loadImages(this.attr.lazyLoadThreshold);
     };
     this.lazyLoad = function() {
       return this.loadImages();
     };
-    this.setupImageErrorHandler = function() {
+    this.loadImages = function(num) {
       var errorUrl;
-      if (this.attr.errorUrl == null) {
-        return;
-      }
       errorUrl = this.attr.errorUrl;
-      return this.$node.find('img').on('error', function() {
-        return this.src = errorUrl;
+      return this.$node.find("img[data-src]").slice(0, num).each(function() {
+        var img;
+        img = $(this);
+        if (errorUrl) {
+          img.on('error', function() {
+            return this.src = errorUrl;
+          });
+        }
+        return img.attr('src', img.attr('data-src')).removeAttr('data-src');
       });
     };
     return this.after('initialize', function() {
-      this.setupImageErrorHandler();
       this.on('uiGalleryLazyLoadRequested', this.lazyLoad);
       return this.initialLoad();
     });
